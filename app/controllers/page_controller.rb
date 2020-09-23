d34335182b017c5ec89d99dfeae632f9c67c98c7
Ishwar Singh Gehlot <gehlotishwar@gmail.com>
2020-09-23 21:54:43 +0530

    update searchign

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -3,8 +3,13 @@ class PageController < ApplicationController
   def index
   	# @channals = Channal.all.includes(:tvshows)
   	@tvshows = Tvshow.all
+
+  	@channalIds = []
+
   	if params[:search].present?
-  		@tvshows = @tvshows.where("(lower(title) like ?)","%#{params[:search].strip.try(:downcase)}%")
+  		@channalIds = Channal.all.where("(lower(title) like ?)","%#{params[:search].strip.try(:downcase)}%").map { |e| e.id  }
+
+  		@tvshows = @tvshows.where("(lower(title) like ?) or tvshows.channal_id in(?)","%#{params[:search].strip.try(:downcase)}%",@channalIds)
   	end
   	@tvshows = @tvshows.order(title: :asc)
   end
