1590bdfebcd31bb98e99abe5c8f60bf67ad6bad4
Ishwar Singh Gehlot <gehlotishwar@gmail.com>
2020-09-23 21:49:35 +0530

    update channal list

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -1,8 +1,12 @@
 class PageController < ApplicationController
   
   def index
-  	@channals = Channal.all.includes(:tvshows)
-  	@channals = @channals.order(title: :asc)
+  	# @channals = Channal.all.includes(:tvshows)
+  	@tvshows = Tvshow.all
+  	if params[:search].present?
+  		@tvshows = @tvshows.where("(lower(title) like ?)","%#{params[:search].strip.try(:downcase)}%")
+  	end
+  	@tvshows = @tvshows.order(title: :asc)
   end
   
 end
