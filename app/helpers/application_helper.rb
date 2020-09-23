33ace0eddb36b30c329cba6511c895d286ee8311
Ishwar Singh Gehlot <gehlotishwar@gmail.com>
2020-09-23 20:17:50 +0530

    update application helper functions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
 
-	def channal_status(type)
-		case type
-		when true
-			return "<span class='badge badge-success'>Active</span>".html_safe
-		when false
-			return "<span class='badge badge-warning'>Disabled</span>".html_safe			
+		def channal_status(type)
+			case type
+			when true
+				return "<span class='badge badge-success'>Active</span>".html_safe
+			when false
+				return "<span class='badge badge-warning'>Disabled</span>".html_safe			
+			end
 		end
-	end
 
 	  def set_Date(dt)
 	    return dt.strftime("%m/%d/%Y")
@@ -21,21 +21,9 @@ module ApplicationHelper
 	    return dt.strftime("%m/%d/%Y %I:%M %p")
 	  end
 
-	  def set_Date_Timzone(dt,current_user)
-	    return dt.strftime("%m/%d/%Y")
-	  end
-
-	  def show_date_with_user(dt,current_user)
-	    return dt.strftime("%d %B, %Y %I:%M %p")
-	  end
-
-	  def show_datetime_with_user(dt,current_user)
-	    return dt.strftime("%d %B, %Y")
-	  end
-
-	  def parse_date(dt)
+	  def parse_date_with_time(dt)
 	    unless dt.to_s.empty?
-	      return DateTime.strptime(dt,'%m/%d/%Y').strftime('%Y/%m/%d')
+	      return DateTime.strptime(dt,'%I:%M %p').strftime('%I:%M %p')
 	    end
 	    return dt
 	  end
