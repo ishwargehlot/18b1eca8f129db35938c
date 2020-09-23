f28a5d6ee4379c63b4e67930a31435628aa659b6
Ishwar Singh Gehlot <gehlotishwar@gmail.com>
2020-09-23 21:42:32 +0530

    update tv show

--- app/models/tvshow.rb
+++ app/models/tvshow.rb
@@ -5,16 +5,13 @@ class Tvshow < ApplicationRecord
    
    # schedular favourite email
    def self.favourite_email_send
-   		@tvshows = Tvshow.where(status: true)
-   		
-   		from_mins_ago = 30.minutes.ago.change(sec: 0)
+   	@tvshows = Tvshow.all.where("status = ? && isfavourite = ?",true,true)
+   	from_mins_ago = 30.minutes.ago.change(sec: 0)
 		to_mins_ago = from_mins_ago.change(sec: 59) 
-		
-	    @tvshows = @tvshows.where(showtime: from_mins_ago..to_min_ago)
-   		if @tvshows.count > 0
-			 TvshowMailer.favourite_email(@tvshows).deliver_now
-   		end
+	   @tvshows = @tvshows.where(showtime: from_mins_ago..to_min_ago)
+		if @tvshows.count > 0
+		   TvshowMailer.favourite_email(@tvshows).deliver_now
+		end
   	end
-
 
 end
